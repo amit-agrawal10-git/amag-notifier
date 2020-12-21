46cf4f374a3c96d84b8e4152c5911bd0ff127056
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-12-21 10:03:51 +0530

    Committing tested code

--- src/main/java/com/github/amag/notifier/NotifierApplication.java
+++ src/main/java/com/github/amag/notifier/NotifierApplication.java
@@ -1,9 +1,7 @@
 package com.github.amag.notifier;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class NotifierApplication {
